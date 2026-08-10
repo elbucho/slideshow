abae8fead804c71d6c27dab77c55fe18cf0f5f49
elbucho <mjhinkson@gmail.com>
2026-08-09 22:57:43 -0700

    Merge pull request #2
    * Set up an abstract controller that matches any HTTP request type and …

--- backends/nest/src/app/app.service.ts
+++ backends/nest/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello There 3';
+    return 'Hello There 2';
   }
 }
--- backends/nest/src/main.ts
+++ backends/nest/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app/app.module';
+import { ErrorResponseFilter } from '@/common/error-response.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Catch any exceptions and return them as an ErrorResponse JSON object
+  app.useGlobalFilters(new ErrorResponseFilter());
+
   await app.listen(process.env.PORT ?? 8000);
 }
 
